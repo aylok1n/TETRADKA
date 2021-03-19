a20c1a8616d139a7dc5ed06d2e068e042d05ec6a
aylok1n <nikolya.blog@gmail.com>
2021-03-19 22:38:49 +0400

    скролл+бьюти

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import SectionTittle from "./Components/SectionTittle"
 
 const DATA = [  
   {
-    tittle: 'Ваши ♂ SLAVES ♂',
+    tittle: 'Ваши ♂SLAVES♂',
     data: [
       {
         user:{
@@ -47,35 +47,9 @@ export default function App() {
         renderSectionHeader={({ section: {tittle} }) =>(
           <SectionTittle>{tittle}</SectionTittle>
         )}
-        />
-      {/* <Group
-        items={[
-          {
-            user:{
-              fullname: 'зачем',
-              avatar:
-                'https://sun9-12.userapi.com/impf/P3pNET8JSahutuB950vdnRsufESU_gtH8Rd7Ow/vPeCRb43-L4.jpg?size=1220x1624&quality=96&sign=a6b20974f936a996f80611c0e367a6a9&type=album'
-            }
-          },
-          {
-            user:{
-              fullname: 'эти',
-              avatar:
-                'https://sun7-9.userapi.com/s/v1/ig2/Ix1lA1CFGwWFdEf42Ebpe_SaYNYLm3BpgGau7LKoZAdzJVLvz5wxq4SBqF4OgUFBJ6ojJqZONrlEUoJ4_d5FCNcA.jpg?size=100x0&quality=96&crop=354,311,725,725&ava=1'
-            }
-          },
-          {
-            user:{
-              fullname: 'фотки',
-              avatar:
-                'https://sun7-6.userapi.com/s/v1/ig2/8H5W5jJgCqI0wIb6bo6YRr3LpWhmaktSosVqj_IiHamQNr99HeEF-WJrkxZpKZlme6v9rGFRLm1UKddTqFkSJMte.jpg?size=50x0&quality=96&crop=2,86,1616,1616&ava=1'
-            }
-          },
-          
-        ]}
-      ></Group> */}
-       <PlusButton style ={{shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.5,shadowRadius: 2.5,elevation: 5,}}>
-       <Ionicons name="ios-add" size={36} color="white" />
+      />
+      <PlusButton style ={{shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.5,shadowRadius: 2.5,elevation: 5,}}>
+        <Ionicons name="ios-add" size={36} color="white" />
       </PlusButton>
     </Container> 
   );
