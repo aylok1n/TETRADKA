91d2741780509fcd2f94a0804056d15c86ce94ac
aylok1n <nikolya.blog@gmail.com>
2021-03-19 07:08:08 +0400

    sdfsd

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up A sdfpp.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> 
   );
 }
 
