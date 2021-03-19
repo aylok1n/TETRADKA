335f851dd52a4cb194004a8e727364cee1ad3adf
aylok1n <nikolya.blog@gmail.com>
2021-03-19 07:44:08 +0400

    dsf

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up A sdfpp.js to start working on your app!</Text>
+      <Text>НУ ЧЕ РОДНОЙ. ПОНЕСЛАСЬ ЕБАТБ</Text>
       <StatusBar style="auto" />
     </View> 
   );
