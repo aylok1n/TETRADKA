fad3152d443cc7a0c1c7e9d8787f6d2cba5937de
aylok1n <nikolya.blog@gmail.com>
2021-03-20 17:25:14 +0400

    выа

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const AppNavigator = createStackNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
-      title: 'TETsddzxdczsds',
+      title: 'TETsddzxdczsываываываds',
     },
   },
   YourBooks: {
