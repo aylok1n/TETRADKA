7be3199bb79758360b95a8705e27503a4ccd6ae8
aylok1n <nikolya.blog@gmail.com>
2021-03-20 17:50:24 +0400

    скролл+ скрины

--- Components/SectionTittle.jsx
+++ Components/SectionTittle.jsx
@@ -4,8 +4,8 @@ export default styled.Text`
 font-weight: 800;
 color: #000000;
 font-weight: bold;
-font-size: 35px;
-margin-left: 10px;
-padding: 5px 0px;
+font-size: 25px;
+
+padding: 5px 15px;
 
 `;
