d4d9e181664ad99fd52cc2789f9e10da3e9b93be
aylok1n <nikolya.blog@gmail.com>
2021-03-21 21:26:39 +0400

    выаыва

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -14,7 +14,7 @@ const DATA = [
       data: [
         {
           user:{
-            fullname: 'зачем',
+            fullname: 'зав чем',
             avatar:
               'https://sun9-12.userapi.com/impf/P3pNET8JSahutuB950vdnRsufESU_gtH8Rd7Ow/vPeCRb43-L4.jpg?size=1220x1624&quality=96&sign=a6b20974f936a996f80611c0e367a6a9&type=album'
           }
